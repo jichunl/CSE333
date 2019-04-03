dc38309a6419d5a501f385e544d92ceffdb16632
Jichun Li <jichunli@uw.edu>
2019-04-03 00:25:13 -0700

    ex0 complete

--- ex/ex0/ex0.c
+++ ex/ex0/ex0.c
@@ -1,4 +1,5 @@
-// CSE 333	Spring 2019
+// Copyright [2019] <Jichun Li> [copyright]
+// CSE 333  Spring 2019
 // ex0
 // jichunli@uw.edu
 
@@ -7,36 +8,39 @@
 #include <stdint.h>
 #include <string.h>
 
-double pi_calc (int64_t n);
+double pi_calc(int32_t n);
 
 int main(int argc, char* argv[]) {
-	// check if argument count is valid 
-	if (argc != 2) {
-    	printf("ERROR: Only 2 arguments allowed\n");
-		exit(EXIT_FAILURE);
+    // check if argument count is valid
+    if (argc != 2) {
+        printf("ERROR: Only 2 arguments allowed\n");
+        exit(EXIT_FAILURE);
     }
 
-	const char* num_char = argv[1];
+    const char* num_char = argv[1];
 
-	// check if argument value is valid
-	for (int i = 0; i < strlen(argv[1]); i++) {
-		if (num_char[i] < 48 || num_char[i] > 57) {	// char is not a number
-			printf("ERROR: Non-numerical char in 2nd argument\n");
-			exit(EXIT_FAILURE);
-		}
-	}
+    // check if argument value is valid
+    while (*num_char != '\0') {
+        if (*num_char < '0' || *num_char > '9') {   // char is not a number
+            printf("ERROR: Non-numerical char in 2nd argument\n");
+            exit(EXIT_FAILURE);
+        }
+        num_char++;
+    }
 
-    int64_t term = (int64_t)&argv[1];
-	double pi = calc_pi(term);
+    int32_t term = atoi(argv[1]);
+    double pi = pi_calc(term);
     printf("Our estimate of Pi is %.20lf\n", pi);
+    exit(EXIT_SUCCESS);
 }
 
-double pi_calc (int64_t n) {
-	double pi = 3;
-	int64_t sign = -1;
-    for (int64_t i = 1; i <= n; i++) {
-		sign = sign * -1;
-		pi = sign * 4 / (2 * n * 2 * (2 * n + 1) * (2 * n + 2)) + pi;
-	}
-	return pi;
+// Calculate pi value
+double pi_calc(int32_t n) {
+    double pi = 3;
+    int32_t sign = -1;
+    for (int32_t i = 1; i <= n; i++) {
+        sign = sign * -1;
+        pi = sign * 4.0 / ((2 * i) * (2 * i + 1) * (2 * i + 2)) + pi;
+    }
+    return pi;
 }
