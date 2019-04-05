3e49282be006f3a456cbab823915c1acb346d085
Jichun Li <jichunli@uw.edu>
2019-04-05 05:56:40 -0700

    ex1

--- ex/ex1/ex1a.c
+++ ex/ex1/ex1a.c
@@ -1,6 +1,6 @@
 // Copyright [2019] <Jichun Li> [copyright]
 // CSE 333 Spring 2019
-// ex1
+// ex1a
 // jichunli@uw.edu
 
 // This is a C program that tests whether or not the following data types are
@@ -15,6 +15,7 @@ static void double_modifier(double src);
 static void array_modifier(int32_t src[]);
 
 int main(int argc, char* argv[]) {
+    // test if double is pass-by-value or pass-by-ref
     double double_src = 1.0;
     double_modifier(double_src);
     if (double_src == 1.0) {
@@ -23,6 +24,7 @@ int main(int argc, char* argv[]) {
         printf("double: pass-by-reference\n");
     }
 
+    // test if int array is pass-by-value or pass-by-ref
     int32_t array_src[3] = {1, 2, 3};
     array_modifier(array_src);
     if (array_src[0] == 1 && array_src[1] == 2 && array_src[2] == 3) {
@@ -32,10 +34,12 @@ int main(int argc, char* argv[]) {
     }
 }
 
+// modifies a double variable
 static void double_modifier(double src) {
     src++;
 }
 
+// modifies a int array variable
 static void array_modifier(int32_t src[]) {
     src++;
 }
---
+++ ex/ex1/ex1b.c
@@ -0,0 +1,60 @@
+// Copyright [2019] <Jichun Li> [copyright]
+// CSE 333 Spring 2019
+// ex1b
+// jichunli@uw.edu
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+static void CopyAndSort(int32_t src[], int32_t dest[], uint32_t length);
+static void PrintArray(int32_t array[], uint32_t length);
+static void Swap(int32_t array[], uint32_t index);
+
+int main(int argc, char** argv) {
+    int32_t src[11] = {3, 2, -5, 7, 13, 4, 1, 7, 8, -8, 6};
+    int32_t dest[11];
+    uint32_t length = 11;
+    
+    // sort and copy result to dest
+    CopyAndSort(src, dest, length);
+
+    // print dest
+    PrintArray(dest, length);
+    return EXIT_SUCCESS;
+}
+
+// This function accepts two arrays of int32_t's, and an array length iterates
+// through the entries of the first array and use insertion sort to insert the
+// entries into the second array, in non-decreasing sorted order.
+static void CopyAndSort(int32_t src[], int32_t dest[], uint32_t length) {
+    for (uint32_t i = 0; i < length; i++) {
+        dest[i] = src[i];
+        Swap(dest, i);
+    }
+}
+
+// This function is a helper function for CopyAndSort. It takes in an int32_t
+// array and uint32_t index which represents the max index in dest array. Ot
+// would recursively swap the entries in the array from back to front until max
+// index is 0.
+static void Swap(int32_t array[], uint32_t index) {
+    if (index > 0) {
+        int32_t curr = array[index];
+        int32_t prev = array[index - 1];
+        if (curr < prev) {
+            array[index] = prev;
+            array[index - 1] = curr;
+            Swap(array, index - 1);
+        }
+    }
+}
+
+// function for print out an array
+static void PrintArray(int32_t array[], uint32_t length) {
+    for (uint32_t i = 0; i < length; i++) {
+        printf("%" PRId32 " ", array[i]);
+    }
+    printf("\n");
+}
